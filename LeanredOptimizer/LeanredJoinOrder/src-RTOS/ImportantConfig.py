5387aa48ed95c8efdc1e31be739f3685aef20e49
allennia <95661475+allennia@users.noreply.github.com>
2021-12-21 02:32:50 -0500

    Update ImportantConfig.py

--- LeanredOptimizer/LeanredJoinOrder/src-RTOS/ImportantConfig.py
+++ LeanredOptimizer/LeanredJoinOrder/src-RTOS/ImportantConfig.py
@@ -4,8 +4,8 @@ class Config:
         self.JOBDir = "Queries/JOB"
         self.schemaFile = "schema.sql"
         self.dbName = "imdbload"
-        self.userName = "dbuser"
-        self.password = "123456"
+        self.userName = "postgres"
+        self.password = "postgres"
         self.ip = "127.0.0.1"
         self.port = 5432
 
